cc83e100edfc3b0a63adf9ba6fa3c12a52a424eb
Chao Liu <liuchao8989@gmail.com>
2017-01-12 21:02:24 +0800

    show all users moods in Other moods

--- public/angular-app/controllers/main.controller.js
+++ public/angular-app/controllers/main.controller.js
@@ -9,26 +9,22 @@ function MainController($scope) {
 
 moodApp.controller('OwnMoodController', OwnMoodController)
 
-function OwnMoodController($window, $scope, $http, AuthFactory) {
+function OwnMoodController($window, $scope, $http, AuthFactory, MoodServices) {
   // when in this page, must logged in
   var self = this;
+  $scope.id = 'ownchart';
   var moodUrl = '/api/users/' + AuthFactory.currentUserId + '/moods';
 
   $scope.isLoggedIn = function() {
     return AuthFactory.isLoggedIn;
   }
 
-  genMoodPoint = function(mood) {
-    var moodTime = new Date(mood.created_at);
-    return [moodTime.getTime(), mood.level];
-  }
-
   if (AuthFactory.isLoggedIn) {
     $http.get(moodUrl)
-      .then(function(respond) {
+      .then(function(response) {
         var moodLevelArray = [];
-        respond.data.forEach(function(mood) {
-          var moodPoint = genMoodPoint(mood);
+        response.data.forEach(function(mood) {
+          var moodPoint = MoodServices.genMoodPoint(mood);
           moodLevelArray.push(moodPoint);
         });
         $scope.chartConfig.series.push({
@@ -66,53 +62,50 @@ function OwnMoodController($window, $scope, $http, AuthFactory) {
         console.log(err);
       });
   }
-
-  $scope.chartConfig = {
-    options: {
-      chart: {
-        type: 'line'
-      },
-      tooltip: {
-        style: {
-          padding: 10,
-          fontWeight: 'bold'
-        }
-      }
-    },
-
-    series: [],
-    title: {
-      text: 'your mood'
-    },
-    subtitle: {
-      text: 'subtitle',
-      x: -20
-    },
-    loading: false,
-    xAxis: {
-      title: {
-        text: 'time'
-      },
-      type: 'datetime'
-    },
-    yAxis: {
-      max: 10,
-      min: 0
-    },
-    size: {
-      width: 400,
-      height: 300
-    }
-  };
-
-
-
+  $scope.chartConfig = MoodServices.genHighChartBasicConfig();
 }
+
+
 
 moodApp.controller('OtherMoodsController', OtherMoodsController)
 
-function OtherMoodsController($scope) {
+function OtherMoodsController($http, $scope, AuthFactory, MoodServices) {
   var self = this;
   $scope.title = "scope other";
   this.title = " self other";
+  $scope.chartConfigs = []
+  $scope.moodPointsArray = [];
+  var originalMoodsArray = [];
+  userUrl = '/api/users';
+  $http.get(userUrl).then(function(response) {
+
+    response.data.forEach(function(user) {
+      if (AuthFactory.isLoggedIn) {
+        if (user._id !== AuthFactory.currentUserId) {
+          originalMoodsArray.push(user.moods);
+        }
+      } else {
+        originalMoodsArray.push(user.moods);
+      }
+    });
+    console.log(originalMoodsArray);
+    originalMoodsArray.forEach(function(moodsForOneUser,index) {
+      var moodPoints = []
+      moodsForOneUser.forEach(function(mood) {
+        moodPoints.push(MoodServices.genMoodPoint(mood));
+      })
+      $scope.moodPointsArray.push(moodPoints);
+      var newChartConfig = MoodServices.genHighChartBasicConfig();
+      newChartConfig.series.push({
+        data: moodPoints
+      });
+      $scope.chartConfigs.push({id:"chart"+index, config:newChartConfig});
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+  // create each config
+
+
 }
---
+++ public/angular-app/services.js
@@ -0,0 +1,53 @@
+console.log("services");
+moodApp.service('MoodServices', MoodServices);
+
+function MoodServices() {
+  this.foo = function() {
+    console.log('bar');
+  }
+
+  this.genMoodPoint = function(mood) {
+    var moodTime = new Date(mood.created_at);
+    return [moodTime.getTime(), mood.level];
+  }
+
+  this.genHighChartBasicConfig = function() {
+    return {
+      options: {
+        chart: {
+          type: 'line'
+        },
+        tooltip: {
+          style: {
+            padding: 10,
+            fontWeight: 'bold'
+          }
+        }
+      },
+      series: [],
+      title: {
+        text: 'your mood'
+      },
+      subtitle: {
+        text: 'subtitle',
+        x: -20
+      },
+      loading: false,
+      xAxis: {
+        title: {
+          text: 'time'
+        },
+        type: 'datetime'
+      },
+      yAxis: {
+        max: 10,
+        min: 0
+      },
+      size: {
+        width: 400,
+        height: 300
+      }
+    };
+
+  }
+}
